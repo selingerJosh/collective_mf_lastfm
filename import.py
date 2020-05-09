51bfad602cc6b63df2de191de6928e3af2e6e361
JSelinger <46336784+selingerJosh@users.noreply.github.com>
2020-05-09 14:06:28 +1000

    added surprise pipeline for recommender training

--- import.py
+++ import.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import surprise as spr
 
-
+# raw import
 df_user_artist = pd.read_csv("data/user_artists.dat", sep = '\\t', engine='python')
 
 df_artist = pd.read_csv("data/artists.dat", sep = '\\t', engine='python')
@@ -10,40 +12,35 @@ df_user_taggedartist = pd.read_csv("data/user_taggedartists.dat", sep = '\\t', e
 
 df_tag = pd.read_csv("data/tags.dat", sep = '\\t', engine='python')
 
-# simple data profiling
-# 1 to 1 relationship of artistID to name
-df_artist.groupby('id').count().sort_values('name', ascending=False)
+# log transform user artist interactions
+df_user_artist['weight_log'] = np.log(df_user_artist['weight'])
 
-# Report counts of artist on user and play level
-df_user_artist_full = pd.merge(df_user_artist,
-                               df_artist,
-                               how='left',
-                               left_on= 'artistID', right_on='id' )
-
-user_counts = (df_user_artist.
-        groupby('artistID', as_index=False).
-        count().
-        sort_values('userID', ascending=False))
-
-user_counts.describe()
-
-user_counts.plot(kind = 'bar',x = 'artistID', y = 'userID',
-          logy = True, xticks = [],
-          title = 'User Artist Interaction Counts')
-plt.show()
-
-play_counts = (df_user_artist.
-        groupby('artistID', as_index=False).
-        sum().
-        sort_values('weight', ascending=False))
-
+# summary stats user artist interactions
 df_user_artist.describe()
 
-play_counts.plot(kind = 'bar',x = 'artistID', y = 'weight',
-          logy = True, xticks = [],
-          title = 'User Artist Interaction Counts')
-plt.show()
+# create user artist interaction matrix NOT NECESSARY WITH SURPRISE
+# df_user_artist.pivot(index = 'userID', columns= 'artistID', values = 'weight_log')
 
+# SIMPLE MODELS only user-item matrix
+# load user artist matrix into surprise
 
+# define rating scale as min to max log user artist interaction count to nearest int
+reader = spr.Reader(rating_scale=(0,13))
 
-df_user_artist_full.groupby('name', as_index=False).count().sort_values('userID', ascending=False, )['userID']
+df_user_artist_spr = spr.Dataset.load_from_df(df_user_artist[['userID','artistID','weight_log']], reader)
+
+# simple data split
+train, test = spr.model_selection.train_test_split(df_user_artist_spr, test_size=.20)
+
+# We'll use the famous SVD algorithm.
+algo = spr.SVD()
+algo_other = spr.prediction_algorithms.matrix_factorization.NMF(biased = True, verbose = True)
+# Train the algorithm on the trainset, and predict ratings for the testset
+algo.fit(train)
+algo_other.fit(train)
+predictions = algo.test(test)
+predictions_other = algo_other(test)
+
+# Then compute RMSE
+spr.accuracy.rmse(predictions)
+spr.accuracy.rmse(predictions_other)
